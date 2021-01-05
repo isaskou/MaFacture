a65dc31ec88d3c0cbf6c84f1c9efec6f72df0d00
isaskou <isabel.skou@yahoo.fr>
2021-01-05 12:16:58 +0100

    suppression de dossiers

---
+++ DAL/Interface/IContactRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MaFacture.Interface
+{
+    public class IContactRepository
+    {
+        IEnumerable<DTO.User> GetAll();
+        DTO.User GetOne(int id);
+        int Insert(DTO.User u);
+        void Update(DTO.User u);
+        bool Delete(int id);
+    }
+}
---
+++ Database1/Stored Procedure/SP_CheckPassword.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[SP_CheckPassword]
+	@mail nvarchar(200),
+	@pwd nvarchar(16)
+
+AS
+	SELECT [UserId]
+		FROM Utilisateur
+		WHERE @mail = [Email] AND HASHBYTES ('SHA2_256', [Salt]+@pwd) = [Password]
+RETURN 0
