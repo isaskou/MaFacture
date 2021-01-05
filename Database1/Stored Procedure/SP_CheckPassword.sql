eca12d4cee37ec0aced863f7d207ff62691caa5b
isaskou <isabel.skou@yahoo.fr>
2021-01-05 17:06:43 +0100

    Revert "suppression de dossiers"
    This reverts commit a65dc31ec88d3c0cbf6c84f1c9efec6f72df0d00.

--- Database1/Stored Procedure/SP_CheckPassword.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE PROCEDURE [dbo].[SP_CheckPassword]
-	@mail nvarchar(200),
-	@pwd nvarchar(16)
-
-AS
-	SELECT [UserId]
-		FROM Utilisateur
-		WHERE @mail = [Email] AND HASHBYTES ('SHA2_256', [Salt]+@pwd) = [Password]
-RETURN 0
